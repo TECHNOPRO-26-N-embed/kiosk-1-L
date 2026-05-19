5c729ea95b5833838fa533dfb9a0b58c32de80ee
Kiyoto-Hosoyama-TP <282411260+Kiyoto-Hosoyama-TP@users.noreply.github.com>
2026-05-19 10:07:42 +0900

    支払いの計算で、金額が不足している場合に再度入力を促すように変更しました。 また、購入した商品名と個数を表示する関数の説明文を修正しました

--- src/Hosoyama.c
+++ src/Hosoyama.c
@@ -12,27 +12,23 @@ int pay(int price, int money) {
     if (money < price) {
         int shortage = price - money;
         printf("金額が不足しています。あと%d円必要です。\n", shortage);
-        return shortage;
+        pay(price, money); // 不足している金額を再度入力させるために再帰呼び出し
     } else {
         printf("お支払いありがとうございます。\n");
     }
-    //buy_item_name(NULL, 0); // 商品名と個数の入力を促す関数を呼び出す
+    
     return 0;
 }
 
 /// @brief 購入した商品名と個数を表示する関数
-/// @brief (例として入力を受け付けています。後々は引数で受け取るように変更予定です。)
+/// @brief (後々は引数で受け取るように変更予定です。)
 /// @param item_name 商品名
 /// @param num 個数
 /// @return なし
 void buy_item_name(char*item_name,int num) {
-    printf("例として入力を受け付けています。後々は引数で受け取るように変更予定です。\n");
-    scanf("購入する商品名を入力してください。: %s", item_name);
-    scanf("購入する個数を入力してください。: %d", &num);
+    printf("後々商品名と個数を引数で受け取るように変更予定です。\n");
     printf("%sを%d個購入しました。\n", item_name, num);
     
-    
-    //printf("%sを%d個購入しました。\n", item_name, num);
 }
 
 void change(int price, int money) {
